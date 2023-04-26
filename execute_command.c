d5f9ae76ee41d9a006ce0421b27402ef911689f3
AMR <amrhoss96@gmail.com>
2023-04-27 01:40:44 +0200

    betty check

--- execute_command.c
+++ execute_command.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
  * execute_external_command - executes commands
- * @args - inputted arguments
+ * @args: inputted arguments
 */
 void execute_external_command(char **args)
 {
+	pid_t pid = fork();
 	char *path = getenv("PATH");
 	char *token = strtok(path, ":");
 	char *full_path = malloc(MAX_INPUT_SIZE * sizeof(char));
+
 	while (token != NULL)
 	{
 		strcpy(full_path, token);
@@ -15,26 +17,25 @@ void execute_external_command(char **args)
 		strcat(full_path, args[0]);
 		if (access(full_path, X_OK) == 0)
 		{
-		pid_t pid = fork();
-		if (pid == 0)
-		{
-			execv(full_path, args);
-			perror("execv failed");
-			exit(EXIT_FAILURE);
-		} 
-		else if (pid < 0)
-		{
-			perror("fork failed");
-			exit(EXIT_FAILURE);
-		} 
-		else
-		{
-			waitpid(pid, NULL, 0);
-			free(full_path);
-			return;
+			if (pid == 0)
+			{
+				execv(full_path, args);
+				perror("execv failed");
+				exit(EXIT_FAILURE);
+			}
+			else if (pid < 0)
+			{
+				perror("fork failed");
+				exit(EXIT_FAILURE);
+			}
+			else
+			{
+				waitpid(pid, NULL, 0);
+				free(full_path);
+				return;
+			}
 		}
-		}
-		token = strtok(NULL, ":");
+	token = strtok(NULL, ":");
 	}
 	free(full_path);
 	printf("%s: command not found\n", args[0]);
