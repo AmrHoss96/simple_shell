69956f22f780b7aac32e4f3429fb7c81d106b292
AMR <amrhoss96@gmail.com>
2023-04-27 01:44:31 +0200

    betty check

--- tokenize_line.c
+++ tokenize_line.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * tokenize_line - tokenize line
- * @line - inputed line
+ * @line: inputed line
  * Return: tokens
 */
-char** tokenize_line(char* line)
+char **tokenize_line(char *line)
 {
 	int i = 0;
-	char** tokens = malloc(MAX_NUM_TOKENS * sizeof(char*));
-	char* token = strtok(line, " \t\n\r");
+	char **tokens = malloc(MAX_NUM_TOKENS * sizeof(char *));
+	char *token = strtok(line, " \t\n\r");
+
 	while (token != NULL)
 	{
 		tokens[i++] = token;
