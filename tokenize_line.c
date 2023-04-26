82fb08ab25d186e039b39abd01dedd09ffb116d6
AMR <amrhoss96@gmail.com>
2023-04-27 01:14:58 +0200

    add tokenizing

---
+++ tokenize_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * tokenize_line - tokenize line
+ * @line - inputed line
+ * Return: tokens
+*/
+char** tokenize_line(char* line)
+{
+	int i = 0;
+	char** tokens = malloc(MAX_NUM_TOKENS * sizeof(char*));
+	char* token = strtok(line, " \t\n\r");
+	while (token != NULL)
+	{
+		tokens[i++] = token;
+		token = strtok(NULL, " \t\n\r");
+	}
+	tokens[i] = NULL;
+	return (tokens);
+}
