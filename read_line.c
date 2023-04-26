d02617a03a6ae9b53248cb6990530c746aaf3faf
AMR <amrhoss96@gmail.com>
2023-04-27 01:43:03 +0200

    betty check

--- read_line.c
+++ read_line.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
  * read_line - reads line into stdin
-*/
-char* read_line(void)
+ * Return: line
+ */
+char *read_line(void)
 {
-	char* line = NULL;
+	char *line = NULL;
 	size_t bufsize = 0;
 	ssize_t nread = getline(&line, &bufsize, stdin);
+
 	if (nread == -1)
 	{
 		printf("\n");
