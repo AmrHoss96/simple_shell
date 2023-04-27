ab38c07f4216dfc10bab76f9c4d1c68fa453a0c1
AMR <amrhoss96@gmail.com>
2023-04-27 02:40:05 +0200

    updating file

--- execute_command.c
+++ execute_command.c
@@ -5,7 +5,6 @@
 */
 void execute_external_command(char **args)
 {
-	pid_t pid = fork();
 	char *path = getenv("PATH");
 	char *token = strtok(path, ":");
 	char *full_path = malloc(MAX_INPUT_SIZE * sizeof(char));
@@ -15,6 +14,8 @@ void execute_external_command(char **args)
 		strcpy(full_path, token);
 		strcat(full_path, "/");
 		strcat(full_path, args[0]);
+		pid_t pid = fork();
+
 		if (access(full_path, X_OK) == 0)
 		{
 			if (pid == 0)
