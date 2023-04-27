cdb97bc4cd552d673aae44276c2fa19ff0f1c6a9
AMR <amrhoss96@gmail.com>
2023-04-27 03:01:58 +0200

    updatting file

--- execute_command.c
+++ execute_command.c
@@ -5,7 +5,6 @@
 */
 void execute_external_command(char **args)
 {
-	pid_t pid = fork();
 	char *path = getenv("PATH");
 	char *token = strtok(path, ":");
 	char *full_path = malloc(MAX_INPUT_SIZE * sizeof(char));
@@ -15,26 +14,28 @@ void execute_external_command(char **args)
 		strcpy(full_path, token);
 		strcat(full_path, "/");
 		strcat(full_path, args[0]);
-	if (access(full_path, X_OK) == 0)
-	{
-		if (pid == 0)
+		pid_t pid = fork();
+		
+		if (access(full_path, X_OK) == 0)
 		{
-		execv(full_path, args);
-		perror("execv failed");
-		exit(EXIT_FAILURE);
+			if (pid == 0)
+			{
+			execv(full_path, args);
+			perror("execv failed");
+			exit(EXIT_FAILURE);
+			}
+			else if (pid < 0)
+			{
+			perror("fork failed");
+			exit(EXIT_FAILURE);
+			}
+			else
+			{
+			waitpid(pid, NULL, 0);
+			free(full_path);
+			return;
+			}
 		}
-		else if (pid < 0)
-		{
-		perror("fork failed");
-		exit(EXIT_FAILURE);
-		}
-		else
-		{
-		waitpid(pid, NULL, 0);
-		free(full_path);
-		return;
-		}
-	}
 	token = strtok(NULL, ":");
 	}
 
