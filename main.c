10dd6202a983e2f43e06452f5fea911f1a4f128d
AMR <amrhoss96@gmail.com>
2023-04-27 05:12:45 +0200

    betty

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include "main.h"
 /**
  * main - entry point of the shell
+ * @argc: argument count
+ * @argv: argument vector
  * Return: 0 on success, -1 on failure
  */
-int main(void)
+int main(int argc, char **argv)
 {
+	(void)argc;
+	(void)argv;
+
 	run_shell();
+
 	return (0);
 }
